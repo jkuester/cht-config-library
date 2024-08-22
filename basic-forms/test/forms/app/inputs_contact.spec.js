0cbcc11f5214b3d2e0dcb47e2b6c87c28282ce7b
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-08-22 11:06:10 -0500

    Add basic forms for loading contact data via contact_selector and accessing the contact data in the inputs

---
+++ basic-forms/test/forms/app/inputs_contact.spec.js
@@ -0,0 +1,73 @@
+const { expect } = require('chai');
+const { harness } = require('../../../../util/test-harness');
+
+const form = 'inputs_contact';
+
+const CONTACT = {
+  _id: 'patient_id',
+  name: 'Current Patient',
+};
+
+describe('Inputs Contact form', () => {
+  it('uses provided contact when opening form from a contact profile page', async () => {
+    const {
+      errors,
+      report: { fields },
+      additionalDocs
+    } = await harness.fillForm(
+      { 
+        form, 
+        subject: CONTACT,
+        content: {} // For some reason the harness is defaulting source to 'action' when no content is provided
+      },
+      [],
+    );
+
+    expect(errors).to.be.empty;
+    expect(additionalDocs).to.be.empty;
+    expect(fields).excluding(['meta']).to.deep.equal({
+      intro: '',
+      inputs: {
+        source: 'contact',
+        contact: CONTACT,
+        meta: { location: { error: '', lat: '', long: '', message: '', }, },
+      },
+      patient_uuid: CONTACT._id,
+      contact_name: CONTACT.name,
+      hello: '',
+    });
+  });
+
+  it('loads selected contact when opening form from reports page', async () => {
+    const {
+      errors,
+      report: { fields },
+      additionalDocs
+    } = await harness.fillForm(
+      {
+        form,
+        subject: null,
+        content: {}
+      },
+      [],
+    );
+
+    expect(errors).to.be.empty;
+    expect(additionalDocs).to.be.empty;
+    // The harness currently does not support using the db-object widget to load a contact doc. So, cannot assert much.
+    expect(fields).excluding(['meta']).to.deep.equal({
+      intro: '',
+      inputs: {
+        source: 'user',
+        contact: {
+          _id: '',
+          name: '',
+        },
+        meta: { location: { error: '', lat: '', long: '', message: '', }, },
+      },
+      patient_uuid: '',
+      contact_name: '',
+      hello: '',
+    });
+  });
+});
---
+++ basic-forms/test/forms/app/select_contact.spec.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai');
+const { harness } = require('../../../../util/test-harness');
+
+const form = 'select_contact';
+
+const CONTACT = {
+  _id: 'patient_id',
+  name: 'Current Patient',
+};
+
+describe('Select Contact form', () => {
+  it('loads contact data both via calculation and user selection', async () => {
+    const {
+      errors,
+      report: { fields },
+      additionalDocs
+    } = await harness.fillForm(
+      { 
+        form,
+        userSettingsDoc: { contact_id: CONTACT._id },
+        content: { contact_page: { contact: CONTACT } } // Can simulate user selection by providing contact data
+      },
+      [],
+    );
+
+    expect(errors).to.be.empty;
+    expect(additionalDocs).to.be.empty;
+    expect(fields).excluding(['meta']).to.deep.equal({
+      intro: '',
+      inputs: {
+        meta: { location: { error: '', lat: '', long: '', message: '', }, },
+        user: {
+          contact_id: 'patient_id'
+        }
+      },
+      user_page: {
+        user_contact: {
+          _id: 'patient_id',
+          name: '', // The rest of the data is not loaded because the db-object widget is not supported by the harness
+          role: '',
+        },
+        user_name: '',
+        user_role: '',
+        description: '',
+      },
+      contact_page: {
+        contact: CONTACT,
+        selected_contact_name: CONTACT.name,
+        description: '',
+      },
+    });
+  });
+});
