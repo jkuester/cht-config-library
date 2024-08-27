13c75fea94eaf98cb07f66870fad138a116e1cb2
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-08-27 14:32:04 -0500

    Adds simple case_id workflow example config

---
+++ case-id/test/forms/app/case_event.spec.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const { harness } = require('../../../../util/test-harness');
+
+const form = 'case_event';
+
+describe('Case Event form', () => {
+  it('submits successfully', async () => {
+    const {
+      errors,
+      report: { fields },
+      additionalDocs
+    } = await harness.fillForm({ form });
+
+    expect(errors).to.be.empty;
+    expect(additionalDocs).to.be.empty;
+    expect(fields).excluding(['meta']).to.deep.equal({ case_id: '' });
+  });
+});
